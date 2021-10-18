b98664d660682fa623e4993e09cd34fc527c494d
Paul Thurmond <paulthurmond@gmail.com>
2021-10-18 06:25:00 -0500

    Added IDs

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 let myLibrary = [];
 let container = document.getElementById("container");
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, id) {
     this.title = title;
     this.author =author;
     this.pages = pages;
     this.read = read;
+    this.id = id;
 };
 
 Book.prototype.info = function() {
@@ -15,15 +16,19 @@ Book.prototype.info = function() {
         } else {
             infoString += ' not read yet';
         }
+        infoString += `, id = ${this.id}`;
         return infoString;
 };
 
 function addBookToLibrary(newTitle, newAuthor, newPages, newRead) {
-    const newBook = new Book(newTitle, newAuthor, newPages, newRead);
+    const newId = Math.random().toString(16).slice(2);
+    console.log(newId);
+    const newBook = new Book(newTitle, newAuthor, newPages, newRead, newId);
+    console.log(newBook);
     myLibrary.push(newBook);
     const newBookHTML = document.createElement("div");
     const newValues = Object.values(newBook);
-    for (const property in newValues) {
+    for (const property of newValues) {
         const newProperty = document.createElement("h5");
         newProperty.textContent = property;
         newBookHTML.appendChild(newProperty);
@@ -38,4 +43,3 @@ addBookToLibrary('Memories of Ice', 'Steven Erikson', 900, true);
 myLibrary.forEach(book => {
     console.log(book.info());
 });
-
