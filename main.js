f50aef8714362c47f35fd1a71d5b4c704d45043d
Paul Thurmond <paulthurmond@gmail.com>
2021-10-19 06:02:28 -0500

    Added classes to books

--- main.js
+++ main.js
@@ -22,22 +22,31 @@ Book.prototype.info = function() {
 
 function addBookToLibrary(newTitle, newAuthor, newPages, newRead) {
     const newId = Math.random().toString(16).slice(2);
-    console.log(newId);
     const newBook = new Book(newTitle, newAuthor, newPages, newRead, newId);
-    console.log(newBook);
+    
+    // Save new book to library
     myLibrary.push(newBook);
+
+    // Display new book on page. Could be a separate function
     const newBookHTML = document.createElement("div");
     const newValues = Object.values(newBook);
     for (const property of newValues) {
         const newProperty = document.createElement("h5");
         newProperty.textContent = property;
+        if  (property === true) {
+            newProperty.textContent = 'Read? yes';
+        }
+        if (property === false) {
+            newProperty.textContent = 'Read? no';
+        }
         newBookHTML.appendChild(newProperty);
     }
+    newBookHTML.classList.add('book');
     container.appendChild(newBookHTML);
 };
 
 addBookToLibrary('Dune', 'Frank Herbert', 400, true);
-addBookToLibrary('The Hitchhiker\'s Guide to the Galazy', 'Douglas Adams', 250, true);
+addBookToLibrary('The Hitchhiker\'s Guide to the Galaxy', 'Douglas Adams', 250, false);
 addBookToLibrary('Memories of Ice', 'Steven Erikson', 900, true);
 
 myLibrary.forEach(book => {
