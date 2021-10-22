f5526b1131b08efd2f766f46afc713ba0e07f538
Paul Thurmond <paulthurmond@gmail.com>
2021-10-22 06:34:37 -0500

    Added local storage

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-let myLibrary = [];
+let myLibrary = JSON.parse(localStorage.getItem("myLibrary") || "[]");
 let container = document.getElementById("container");
+updateDisplay();
 
 const newBookButton = document.getElementById("newBookButton");
 newBookButton.addEventListener('click', () => {
@@ -12,8 +13,9 @@ form.addEventListener('submit', (e) => {
     let title = document.getElementById('title').value;
     let author = document.getElementById('author').value;
     let pages = document.getElementById('pages').value;
-    let read = document.getElementById('read').value;
+    let read = document.getElementById('read').checked;
     addBookToLibrary(title, author, pages, read);
+    form.reset();
 })
 
 function Book(title, author, pages, read, id) {
@@ -46,28 +48,49 @@ function addBookToLibrary(newTitle, newAuthor, newPages, newRead) {
     // Save new book to library
     myLibrary.push(newBook);
 
-    // Display new book on page. Could be a separate function
-    const newBookHTML = document.createElement("div");
-    const newValues = Object.values(newBook);
-    for (const property of newValues) {
-        const newProperty = document.createElement("h5");
-        newProperty.textContent = property;
-        if  (property === true) {
-            newProperty.textContent = 'Read? yes';
-        }
-        if (property === false) {
-            newProperty.textContent = 'Read? no';
-        }
-        newBookHTML.appendChild(newProperty);
-    }
-    newBookHTML.classList.add('book');
-    container.appendChild(newBookHTML);
+    // Save library to localStorage
+    localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+
+    updateDisplay();
 };
 
-addBookToLibrary('Dune', 'Frank Herbert', 400, true);
-addBookToLibrary('The Hitchhiker\'s Guide to the Galaxy', 'Douglas Adams', 250, false);
-addBookToLibrary('Memories of Ice', 'Steven Erikson', 900, true);
+// Remove all books from DOM, then add them all back.
+// I think React will make this better.
+function updateDisplay() {
+    // Clear all children
+    container.innerHTML = '';
 
-myLibrary.forEach(book => {
-    console.log(book.info());
-});
+    myLibrary.forEach(function (book) {
+        const bookHTML = document.createElement("div");
+        const values = Object.values(book);
+        for (const property of values) {
+            const newProperty = document.createElement("h5");
+            newProperty.textContent = property;
+            if  (property === true) {
+                newProperty.textContent = 'Read? yes';
+            }
+            if (property === false) {
+                newProperty.textContent = 'Read? no';
+            }
+            bookHTML.appendChild(newProperty);
+        }
+        const btn = document.createElement('button');
+        btn.textContent = 'Delete';
+        btn.addEventListener('click', function() {
+            deleteBook(this.title);
+        });
+        bookHTML.appendChild(btn);
+
+        bookHTML.classList.add('book');
+
+        container.appendChild(bookHTML);
+    });
+};
+
+function deleteBook(toBeDeleted) {
+    console.log(toBeDeleted);
+};
+
+// addBookToLibrary('Dune', 'Frank Herbert', 400, true);
+// addBookToLibrary('The Hitchhiker\'s Guide to the Galaxy', 'Douglas Adams', 250, false);
+// addBookToLibrary('Memories of Ice', 'Steven Erikson', 900, true);
